30e7656622b55cc1b0ea080194bfad9fdd3769e5
Iván García Rodríguez <ivan.garcia.rodriguez@hotmail.es>
2022-11-14 22:25:35 +0100

    First use of describe to group tests

--- demos/suma.test.js
+++ demos/suma.test.js
@@ -1,9 +1,11 @@
 const suma = require('./suma');
 
-test('sumar 1 + 2 es igual a 3', () => {
-  expect(suma(1, 2)).toBe(3);
-});
+describe('test de la función suma', () => {
+  test('sumar 1 + 2 es igual a 3', () => {
+    expect(suma(1, 2)).toBe(3);
+  });
 
-test('sumar 0 + 0 debe ser 0', () => {
-  expect(suma(0, 0)).toBe(0);
+  test('sumar 0 + 0 debe ser 0', () => {
+    expect(suma(0, 0)).toBe(0);
+  });
 });
